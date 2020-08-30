899b997482d9d9e1128dfb47ec3de3b1ca3be3d1
Dice-K39 <daisukehashimoto39@gmail.com>
2020-08-30 11:41:16 -0400

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,25 @@
+class Appointment < ApplicationRecord
+    belongs_to :doctor
+    belongs_to :patient
+
+    def doctor_id
+        self.doctor.id
+    end
+
+    def doctor_name
+        self.doctor.name
+    end
+
+    def patient_id
+        self.patient.id
+    end
+
+    def patient_name
+        self.patient.name
+    end
+
+    def appointment_date_and_time
+        appointment_datetime.strftime("%B %d, %Y at %H:%M")
+        # appointment_datetime.to_formatted_s(:long)
+    end
+end
